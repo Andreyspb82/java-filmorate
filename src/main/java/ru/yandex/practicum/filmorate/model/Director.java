ca2f8b2c1e412df0f83bb3969891079a86044101
Nika <nika@w-up.ru>
2023-07-19 17:03:09 +0300

    feat: добавить handleMethodArgumentNotValidException в ErrorHandler, зависимость для валидации аннотациями в pom, DirectorService, DirectorController

--- src/main/java/ru/yandex/practicum/filmorate/model/Director.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Director.java
@@ -1,10 +1,16 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
+@AllArgsConstructor
 public class Director {
 
     private int id;
+    //Проверяем что имя заполнено
+    @NotBlank(message = "Имя не может быть пустым")
     private String name;
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/DirectorDao.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/DirectorDao.java
@@ -2,14 +2,22 @@ package ru.yandex.practicum.filmorate.storage.dao;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Director;
+import ru.yandex.practicum.filmorate.model.Film;
 
 import java.awt.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @Component
 @Slf4j
@@ -29,18 +37,37 @@ public class DirectorDao {
     }
 
     public Director updateDirector( Director director){
-        return null;
+        String sqlUpdate = "update directors set name = ? WHERE id =?;";
+       if(Objects.nonNull(getDirectorById(director.getId()))) {
+       jdbcTemplate.update(sqlUpdate, director.getName(), director.getId());
+       }
+       return director;
     }
 
     public Director getDirectorById(int id){
-        return null;
+        String sqlSelect = "select * from directors where id = ?;";
+        try {
+            return jdbcTemplate.queryForObject(sqlSelect, this::mapRowToDirector, id);
+        } catch (DataAccessException e) {
+            log.info("Режиссер с id = {} не найден", id );
+            throw new NotFoundException("Режиссер с id = "+ id +" не найден");
+        }
     }
 
-    public ArrayList<Director> getAllDirectors(){
-        return null;
+    public List<Director> getAllDirectors(){
+        String sqlSelect = "select * from directors ;";
+        List<Director> directors = new ArrayList<>();
+        directors =  jdbcTemplate.query(sqlSelect,this::mapRowToDirector);
+        return directors;
     }
 
     public boolean deleteDirectorById(int id){
-        return true;
+       String sqlDelete = "delete from directors where id = ?";
+       return jdbcTemplate.update(sqlDelete,id) > 0;
+    }
+
+    private Director mapRowToDirector (ResultSet resultSet, int rowNum) throws SQLException {
+
+        return new Director(resultSet.getInt("id"), resultSet.getString("name"));
     }
 }
