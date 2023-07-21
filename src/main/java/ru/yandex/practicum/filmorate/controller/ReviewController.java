540c93068d794fc4c1d8197a9538399c8f28f9b6
pEvgeniy <popevgeniyv@yandex.ru>
2023-07-21 19:14:24 +0300

    feat: reviews for films

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
@@ -0,0 +1,82 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.model.Review;
+import ru.yandex.practicum.filmorate.service.ReviewService;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import java.util.List;
+
+@Validated
+@RestController
+@RequestMapping("/reviews")
+@AllArgsConstructor
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @PostMapping
+    public Review create(@Valid @RequestBody Review review) {
+        return reviewService.create(review);
+    }
+
+    @PutMapping
+    public Review update(@Valid @RequestBody Review review) {
+        return reviewService.update(review);
+    }
+
+    @DeleteMapping("/{id}")
+    public int delete(@Valid @PathVariable int id) {
+        return reviewService.delete(id);
+    }
+
+    @GetMapping("/{id}")
+    public Review findReviewById(@Valid @PathVariable int id) {
+        return reviewService.findReviewById(id);
+    }
+
+    @GetMapping
+    public List<Review> findReviewsByFilmId(
+            @RequestParam(required = false) Integer filmId,
+            @RequestParam (defaultValue = "10", required = false) @Positive int count) {
+        if (filmId == null) {
+            return reviewService.findAllReviews();
+        }
+        return reviewService.findReviewsByFilmId(filmId, count);
+    }
+
+    @PutMapping("/{id}/like/{userId}")
+    public Review addLike(@Valid @PathVariable int id,
+                          @Valid @PathVariable int userId) {
+        return reviewService.addLike(id, userId);
+    }
+
+    @PutMapping("/{id}/dislike/{userId}")
+    public Review addDislike(@Valid @PathVariable int id,
+                             @Valid @PathVariable int userId) {
+        return reviewService.addDislike(id, userId);
+    }
+
+    @DeleteMapping("/{id}/like/{userId}")
+    public Review deleteLike(@Valid @PathVariable int id,
+                             @Valid @PathVariable int userId) {
+        return reviewService.deleteLike(id, userId);
+    }
+
+    @DeleteMapping("/{id}/dislike/{userId}")
+    public Review deleteDislike(@Valid @PathVariable int id,
+                                @Valid @PathVariable int userId) {
+        return reviewService.deleteDislike(id, userId);
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewDbStorage.java
@@ -0,0 +1,171 @@
+package ru.yandex.practicum.filmorate.storage.review;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.Review;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class ReviewDbStorage implements ReviewStorage {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public Review create(Review review) {
+        if (review.getUserId() < 0 || review.getFilmId() < 0) {
+            throw new NotFoundException("Такой id не существует");
+        }
+        SimpleJdbcInsert insert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("reviews")
+                .usingGeneratedKeyColumns("id");
+        review.setReviewId(insert.executeAndReturnKey(reviewToMap(review)).intValue());
+        return review;
+    }
+
+    @Override
+    public Review update(Review review) {
+        String sqlQuery = "UPDATE reviews " +
+                "SET content=?, is_positive=?" +
+                "WHERE id=? ";
+        int result = jdbcTemplate.update(sqlQuery, review.getContent(), review.isPositive(), review.getReviewId());
+        if (result == 0) {
+            log.error("Отзыв для обновления с id = {} не найден", review.getReviewId());
+            throw new NotFoundException("Отзыв для обновления с id = " + review.getReviewId() + " не найден");
+        }
+        return findReviewById(review.getReviewId());
+    }
+
+    @Override
+    public int delete(int id) {
+        String sqlQuery = "DELETE " +
+                "FROM reviews " +
+                "WHERE reviews.id=? ";
+
+        int result = jdbcTemplate.update(sqlQuery, id);
+        if (result == 0) {
+            log.error("Отзыв для удаления с id = {} не найден", id);
+            throw new NotFoundException("Отзыв для удаления с id = " + id + " не найден");
+        }
+        return id;
+    }
+
+    @Override
+    public List<Review> findAllReviews() {
+        String sqlQuery = "SELECT * " +
+                "FROM reviews";
+        return jdbcTemplate.query(sqlQuery, this::mapRowToReview);
+    }
+
+    @Override
+    public Review findReviewById(int id) {
+        String sqlQuery = "SELECT *" +
+                "FROM reviews " +
+                "WHERE reviews.id=? ";
+        try {
+            return jdbcTemplate.queryForObject(sqlQuery, this::mapRowToReview, id);
+        } catch (DataAccessException e) {
+            log.error("Отзыв с id = {} не найден", id);
+            throw new NotFoundException("Отзыв с id " + id + " не найден");
+        }
+    }
+
+    @Override
+    public List<Review> findReviewsByFilmId(int filmId, int count) {
+        String sqlQuery = "SELECT * " +
+                "FROM reviews " +
+                "WHERE film_id=? " +
+                "LIMIT ?";
+        return jdbcTemplate.query(sqlQuery, this::mapRowToReview, filmId, count);
+    }
+
+    @Override
+    public Review addLike(int id, int userId) {
+        String sqlQuery = "INSERT INTO like_review (review_id, user_id, type) VALUES (?, ?, 1)";
+        int result = jdbcTemplate.update(sqlQuery, id, userId);
+        if (result == 0) {
+            log.error("Отзыв для добавления лайка с id = {} не найден", id);
+            throw new NotFoundException("Отзыв для добавления лайка с id = " + id + " не найден");
+        }
+        return findReviewById(id);
+    }
+
+    @Override
+    public Review addDislike(int id, int userId) {
+        String sqlQuery = "INSERT INTO like_review (review_id, user_id, type) VALUES (?, ?, -1)";
+        int result = jdbcTemplate.update(sqlQuery, id, userId);
+        if (result == 0) {
+            log.error("Отзыв для добавления дизлайка с id = {} не найден", id);
+            throw new NotFoundException("Отзыв для добавления дизлайка с id = " + id + " не найден");
+        }
+        return findReviewById(id);
+    }
+
+    @Override
+    public Review deleteLike(int id, int userId) {
+        String sqlQuery = "DELETE " +
+                "FROM like_review " +
+                "WHERE review_id=? AND user_id=? AND type=1";
+        int result = jdbcTemplate.update(sqlQuery, id, userId);
+        if (result == 0) {
+            log.error("Отзыв для удаления лайка с id = {} не найден", id);
+            throw new NotFoundException("Отзыв для удаления лайка с id = " + id + " не найден");
+        }
+        return findReviewById(id);
+    }
+
+    @Override
+    public Review deleteDislike(int id, int userId) {
+        String sqlQuery = "DELETE " +
+                "FROM like_review " +
+                "WHERE review_id=? AND user_id=? AND type=-1";
+        int result = jdbcTemplate.update(sqlQuery, id, userId);
+        if (result == 0) {
+            log.error("Отзыв для удаления дизлайка с id = {} не найден", id);
+            throw new NotFoundException("Отзыв для удаления дизлайка с id = " + id + " не найден");
+        }
+        return findReviewById(id);
+    }
+
+    private Map<String, Object> reviewToMap(Review review) {
+        Map<String, Object> values = new HashMap<>();
+        values.put("content", review.getContent());
+        values.put("is_positive", review.isPositive());
+        values.put("user_id", review.getUserId());
+        values.put("film_id", review.getFilmId());
+        return values;
+    }
+
+    private Review mapRowToReview(ResultSet rs, int rowNum) throws SQLException {
+        return Review.builder()
+                .reviewId(rs.getInt("id"))
+                .content(rs.getString("content"))
+                .isPositive(rs.getBoolean("is_positive"))
+                .userId(rs.getInt("user_id"))
+                .filmId(rs.getInt("film_id"))
+                .useful(getUseful(rs.getInt("id")))
+                .build();
+    }
+
+    private Integer getUseful(int review_id) {
+        String sqlQuery = "SELECT sum(type) " +
+                "FROM like_review " +
+                "WHERE review_id=? ";
+        Integer likes = jdbcTemplate.queryForObject(sqlQuery, Integer.class, review_id);
+        if (likes == null) {
+            return 0;
+        }
+        return likes;
+    }
+}
