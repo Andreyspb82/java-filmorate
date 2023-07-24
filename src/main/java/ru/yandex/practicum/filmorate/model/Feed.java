5f01d6e6a87709d4ad0f8a8872d4d550a100bd18
Andreyspb82 <amitkin82@yandex.ru>
2023-07-24 11:29:00 +0300

    резерв1

--- src/main/java/ru/yandex/practicum/filmorate/model/Feed.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Feed.java
@@ -1,4 +1,27 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.validation.constraints.PositiveOrZero;
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
 public class Feed {
+
+    @PositiveOrZero
+    private int eventId;
+
+    private Instant timestamp;
+
+    private int userId;
+
+    private String eventType;
+
+    private String operation;
+
+    private int entityId;
+
+
 }
