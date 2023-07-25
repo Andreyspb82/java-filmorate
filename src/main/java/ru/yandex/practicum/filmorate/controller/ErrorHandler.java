5c519cad1cb20c52c84492a09e31eb4014ef0c9f
Andreyspb82 <amitkin82@yandex.ru>
2023-07-25 10:48:11 +0300

    feet: add-feed, реализовать ленту событий v00 - добавление и удаление друзей - добавление и удаление лайков для фильмов - создание, удаление, обновление отзывов

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
